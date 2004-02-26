d48672f4cfa93907a68bfc5d42c3306551c96167
Egbert Eich <eich@suse.de>
2004-02-26 09:23:34 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- src/xf86Calcomp.c
+++ src/xf86Calcomp.c
@@ -31,7 +31,7 @@
  * authorization  from Martin Kroeker or Daveg GmbH.
  *
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/input/calcomp/xf86Calcomp.c,v 1.8 2003/01/15 03:43:58 dawes Exp $ */
+/* $XFree86$ */
 
 #define _CALCOMP_C_
 /*****************************************************************************
